654d898252d7e9c9a4fcb9187f8a8d8450493015
Surovi Adlin Palma <surovi202120005@student.ndub.edu.bd>
2026-07-18 23:34:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ if (noBtn) {
         noBtn.style.transform = `translate(${x}px, ${y}px)`;
     });
 }
+function createHeart() {
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "❤";
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 300);
